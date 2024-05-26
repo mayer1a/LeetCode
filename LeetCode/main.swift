54f84ecf404f8b7dfc868ef3ed6fb668b1f69a87
Artem Mayer <46002217+mayer1a@users.noreply.github.com>
2024-05-26 19:44:38 +0700

    Add solutions to 21 and 102 problems (#4)
    21. Merge Two Sorted Lists (Linked List, recursion)
    102. Binary Tree Level Order Traversal (Tree, Breadth-First Search)

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -9,5 +9,24 @@ import Foundation
 
 final class Solution {
 
+    // MARK: - 21. Merge Two Sorted Lists
+
+    func mergeTwoSortedLists(_ list1: ListNode?,
+                             _ list2: ListNode?,
+                             alternativeSolution: Bool = false) -> ListNode? {
+        
+        let solution = MergeTwoSortedLists_Solution()
+        return alternativeSolution ?
+            solution.mergeTwoLists_v2(list1, list2) :
+            solution.mergeTwoLists_v1(list1, list2)
+    }
+
+    // MARK: - 102. Binary Tree Level Order Traversal
+
+    func binaryTreeLevelOrderTraversal(_ root: TreeNode?, iterative: Bool = true) -> [[Int]] {
+        let solution = BinaryTreeLevelOrderTraversal_Solution()
+        return iterative ? solution.levelOrder_iteratively(root) : solution.levelOrder_recursively(root)
+    }
+
 }
 
