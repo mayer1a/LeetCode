61a8711891308fd97ecd5c138ac8ac62a7b47b2e
mayer1a <mayer1art@gmail.com>
2024-05-28 19:59:32 +0700

    Add solutions to 206 and 283 problems
    206. Reverse Linked List
    283. Move Zeroes

--- LeetCode75/main.swift
+++ LeetCode75/main.swift
@@ -9,5 +9,23 @@ import Foundation
 
 final class Solution {
 
+    // MARK: - 206. Reverse Linked List
+
+    func reverseLinkedList(_ head: ListNode?, iterative: Bool = true) -> ListNode? {
+        let solution = ReverseLinkedList_Solution()
+        return iterative ? solution.reverseList_iterative(head) : solution.reverseList_recursive(head)
+    }
+
+    // MARK: - 283. Move Zeroes
+
+    func moveZeroes(_ nums: inout [Int], alt: Bool = false) {
+        let solution = MoveZeroes_Solution()
+        if alt {
+            return solution.moveZeroes_v2(&nums)
+        } else {
+            return solution.moveZeroes_v1(&nums)
+        }
+    }
+
 }
 
